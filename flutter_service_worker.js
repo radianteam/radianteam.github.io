4c52936d22648466dbe25002a537c011ac9c0016
radianteam <radianlmsteam@gmail.com>
2022-10-02 19:26:20 +0000

    autodeploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "e4018e31ef7be4ff6fc7d8b40c553e71",
-"index.html": "33b9f852fd98e668939a865499eb194d",
-"/": "33b9f852fd98e668939a865499eb194d",
+"main.dart.js": "68fe5b4b5d9263ff3885bc2d301ed10c",
+"index.html": "0240a153ee0cae85bc15d2ccada6c9cd",
+"/": "0240a153ee0cae85bc15d2ccada6c9cd",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "74fd03fff6b610d7e082328262004451",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
